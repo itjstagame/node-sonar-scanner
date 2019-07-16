77f0389a546ee1f0ec14aaa9ce4f843fd7fc615a
itjstagame <christopher.j.werner@gmail.com>
2019-07-16 14:35:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,79 @@
 'use strict';
 
 var child_process = require('child_process'),
-	path = require('path');
+	fs = require('fs'),
+	request = require('request'),
+	path = require('path'),
+	unzip = require('unzip2');
 
 var args = process.argv.slice(2),
-	script = path.join(__dirname, 'bin', 'sonar-scanner'),
-	command;
-
-if (process.platform === 'win32') {
-	command = 'cmd.exe';
-	args = ['/c', (script + '.bat')].concat(args);
-}
-else {
+	sonar = path.join(__dirname, 'sonar'),
+	script = path.join(sonar, 'bin', 'sonar-scanner'),
 	command = script;
+
+var _platform = process.platform;
+switch (_platform) {
+	case 'darwin': _platform = 'macosx'; break;
+	case 'win32': 
+		_platform = 'windows'; 
+		command = 'cmd.exe';
+		args = ['/c', (script + '.bat')].concat(args);
+		break;
+	case 'linux': break;
+	default:
+		fail('unsupported platform: ' + _platform);
+}
+const platform = () => _platform;
+
+const run = () => {
+	var child = child_process.spawn(command, args, {
+		stdio: 'inherit'
+	});
+
+	child.on('close', function(code) {
+		process.exit(code);
+	});
+};
+
+if (!fs.existsSync(sonar)) {
+		const root = 'sonar-scanner-cli-4.0.0.1744-' + platform();
+		const urlStr = 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/' + root + '.zip';
+
+		console.log("Downloading from: ", urlStr);
+		request
+		  .get({
+			url: urlStr,
+			rejectUnauthorized: false,
+			agent: false
+		  })
+		//   .on('response', res => {
+		// 	var len = parseInt(res.headers['content-length'], 10);
+		// 	var bar = new ProgressBar('  downloading and preparing JRE [:bar] :percent :etas', {
+		// 	  complete: '=',
+		// 	  incomplete: ' ',
+		// 	  width: 80,
+		// 	  total: len
+		// 	});
+		// 	res.on('data', chunk => bar.tick(chunk.length));
+		//   })
+		  .on('error', err => {
+			console.log(`problem with request: ${err.message}`);
+		  })
+		  .pipe(unzip.Parse()) //Extract({ path: sonar }))
+		  .on('entry', function (entry) {
+			var fileName = path.join(sonar,entry.path.substring(entry.path.indexOf('/')));
+			var type = entry.type; // 'Directory' or 'File'
+			if(type == 'Directory') {
+				fs.mkdirSync(fileName);
+				entry.autodrain();
+			} else {
+				entry.pipe(fs.createWriteStream(fileName));
+			}
+		  })
+		  .on('close', () => {
+			run();
+		  });
+} else {
+	run();
 }
 
-var child = child_process.spawn(command, args, {
-	stdio: 'inherit'
-});
-
-child.on('close', function(code) {
-	process.exit(code);
-});
